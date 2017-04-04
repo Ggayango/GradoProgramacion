1707f1c6c782e709f2b3692de9e2c509caac6bfe
carlospc <carlospc@carlos>
2017-04-04 13:03:38 +0200

    tercera modificación

--- OmegaAlfaBeta/src/Main.java
+++ OmegaAlfaBeta/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("jajaxd ha sido modificado por angel :D");
+		System.out.println("jajaxd va a cambiar porque yo lo digo :P");
+
 	}
 
 }
