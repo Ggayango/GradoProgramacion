0dee3585a8fdd945f2011202c30166b03d5af38e
carlospc <carlospc@carlos>
2017-04-04 13:08:31 +0200

    cuarta modificación

--- OmegaAlfaBeta/src/Main.java
+++ OmegaAlfaBeta/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("jajaxd ha sido modificado por angel :D");
 		System.out.println("jajaxd va a cambiar porque yo lo digo :P");
+		System.out.println("jajaxd va a cambiar porque yo lo digo y punto :P");
 
 	}
 
