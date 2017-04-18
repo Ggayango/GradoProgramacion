ef20f3a52a3ad243053267430431f1cf6a752f14
Guille <Guille@Guille_PT>
2017-04-18 13:08:36 +0200

    mas cambios

--- 3Game4U/src/LanzarDados.java
+++ 3Game4U/src/LanzarDados.java
@@ -1,82 +1,72 @@
 import java.util.Scanner;
 import java.util.Random;
- 
- public class LanzarDados
- {  // Abre clase LanzarDados
 
- Scanner entrada = new Scanner(System.in);
- Random aleatorio = new Random();
- private int numero;
- int Arreglo[];
- 
- /////////////////////////////////////////////
- // Metodo Recibir 
- /////////////////////////////////////////////
- 
- public void Recibir()
- {  // Abre metodo Recibir
- System.out.print("\nPor favor introduzca el numero de veces que se lanzaran ");
- System.out.print(" los dados\n");
- numero = entrada.nextInt();
- Arreglo = new int[numero];
- Lanzar();
- }  // cierra metodo Recibir
+public class LanzarDados { // Abre clase LanzarDados
 
+	Scanner entrada = new Scanner(System.in);
+	Random aleatorio = new Random();
+	private int numero;
+	int Arreglo[];
 
- /////////////////////////////////////////////
- // Metodo Lanzar
- /////////////////////////////////////////////
+	/////////////////////////////////////////////
+	// Metodo Recibir
+	/////////////////////////////////////////////
 
- public void Lanzar()
- {  // Abre metodo Lanzar
- int numero1;
- int numero2;
- 
- for ( int i = 0; i < Arreglo.length; i++ )
- {      // Abre for
- numero1 = 1 + aleatorio.nextInt(6); 
- numero2 = 1 + aleatorio.nextInt(6);
- // System.out.printf("\n%d\t%d\n", numero1, numero2);
- // Descomentar para verificar que las sumas se capturan
- // de manera correcta. Para esto intruducir un numero
- // pequenio
- Arreglo[i] = numero1 + numero2; 
- }      // Cierra for 
+	public void Recibir() { // Abre metodo Recibir
+		System.out.print("\nPor favor introduzca el numero de veces que se lanzaran ");
+		System.out.print(" los dados\n");
+		numero = entrada.nextInt();
+		Arreglo = new int[numero];
+		Lanzar();
+	} // cierra metodo Recibir
 
- Contar();
- }  // Cierra metodo Lanzar
+	/////////////////////////////////////////////
+	// Metodo Lanzar
+	/////////////////////////////////////////////
 
- /////////////////////////////////////////////
- // Metodo Contar
- /////////////////////////////////////////////
+	public void Lanzar() { // Abre metodo Lanzar
+		int numero1;
+		int numero2;
 
- public void Contar()
- {     // Abre metodo Contar
- int Contador[] = new int[13];
- 
- for ( int j = 0; j < Arreglo.length; j++ )
- {  // Abre for
- for ( int k = 0; k < Contador.length; k++ )
- {      // Abre for anidado 
- if ( Arreglo[j] == k )
- Contador[k]++;
- }  // Cierra for anidado
- }  // Cierra for
+		for (int i = 0; i < Arreglo.length; i++) { // Abre for
+			numero1 = 1 + aleatorio.nextInt(6);
+			numero2 = 1 + aleatorio.nextInt(6);
+			// System.out.printf("\n%d\t%d\n", numero1, numero2);
+			// Descomentar para verificar que las sumas se capturan
+			// de manera correcta. Para esto intruducir un numero
+			// pequenio
+			Arreglo[i] = numero1 + numero2;
+		} // Cierra for
 
- Imprimir(Contador); 
+		Contar();
+	} // Cierra metodo Lanzar
 
- }     // Cierra metodo Contar
+	/////////////////////////////////////////////
+	// Metodo Contar
+	/////////////////////////////////////////////
 
- /////////////////////////////////////////////
- // Metodo Imprimir 
- /////////////////////////////////////////////
+	public void Contar() { // Abre metodo Contar
+		int Contador[] = new int[13];
 
- public void Imprimir( int B[])
- { // Abre metodo Imprimir
- for (int m = 0; m < B.length; m++ )
- {       // Abre for
- System.out.printf("\n%d lanzamientos sumaron %d\n", B[m], m ); 
- }       // Cierra for
- }  // Cierra metodo Imprimir
+		for (int j = 0; j < Arreglo.length; j++) { // Abre for
+			for (int k = 0; k < Contador.length; k++) { // Abre for anidado
+				if (Arreglo[j] == k)
+					Contador[k]++;
+			} // Cierra for anidado
+		} // Cierra for
 
- }  // Cierra clase LanzarDdos
+		Imprimir(Contador);
+
+	} // Cierra metodo Contar
+
+	/////////////////////////////////////////////
+	// Metodo Imprimir
+	/////////////////////////////////////////////
+
+	public void Imprimir(int B[]) { // Abre metodo Imprimir
+		for (int m = 0; m < B.length; m++) { // Abre for
+			System.out.printf("\n%d lanzamientos sumaron %d\n", B[m], m);
+		} // Cierra for
+	} // Cierra metodo Imprimir
+
+} // Cierra clase LanzarDdos
