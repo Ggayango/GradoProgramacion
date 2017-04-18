72614cf4ec88e0e5102100f2254382d7d912d47d
angad <angad@DESKTOP-T7HM9C2>
2017-04-18 12:11:27 +0200

    Tengo este error compañeros programadores, a ver si podeis ayudarme.

--- 3Game4U/src/NumerosAleatorios.java
+++ 3Game4U/src/NumerosAleatorios.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
 public class NumerosAleatorios {
 	
+	Scanner in = new Scanner(System.in);
+	  int numero;
+	  int valor;
+	  int contador=0;
+	  boolean ganar=false;
 	
-
+	  System.out.println("--------------------------------------");
+	  System.out.println("|         Adivina el número          |");
+	  System.out.println("--------------------------------------");
+	
 }
+
