c189b08fd010a0d00578630ff32a6b826907f44c
Guille <Guille@Guille_PT>
2017-04-18 13:06:51 +0200

    cambios en mi clase y en el main

--- 3Game4U/src/LanzarDados.java
+++ 3Game4U/src/LanzarDados.java
@@ -80,18 +80,3 @@ import java.util.Random;
  }  // Cierra metodo Imprimir
 
  }  // Cierra clase LanzarDdos
-
-
-
-
-
-public class Main {
-		
-		 public static void main(String args[]){
-			 
-		LanzarDados miDados = new LanzarDados();
-		  
-		 System.out.println("Este programa simula el lanzamiento de dos dados.");
-		 miDados.Recibir();		 
-	}		 
-} 
--- 3Game4U/src/Main.java
+++ 3Game4U/src/Main.java
@@ -1,9 +1,10 @@
-
 public class Main {
-
-	public static void main(String[] args) {
 		
-		
-	}
-
-}
+		 public static void main(String args[]){
+			 
+		LanzarDados miDados = new LanzarDados();
+		  
+		 System.out.println("Este programa simula el lanzamiento de dos dados.");
+		 miDados.Recibir();		 
+	}		 
+} 
