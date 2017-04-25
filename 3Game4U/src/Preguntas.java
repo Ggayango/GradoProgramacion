90cd361bfd079c18e017515914391f1be60394bb
carlospc <carlospc@carlos>
2017-04-25 12:49:24 +0200

    cambios con comentarios

--- 3Game4U/src/Preguntas.java
+++ 3Game4U/src/Preguntas.java
@@ -1,23 +1,39 @@
-
 //Lo primero que haremos será importar el paquete de lectura.
 import java.util.Scanner; //Este es el paquete de lectura
 //Ahora vamos a poner el nombre de la clase
 
-class Preguntas { // recuerden que el nombre de la clase es en mayúscula
-	public static void main(String... Args) { // Este es el método principal
-		
-		// Declaramos algunas variables
-		char res; // esta nos servirá para guardar la respuesta
+/**
+ * <h2>	Juego preguntas </h2>
+ * En esta clase presentamos el juego preguntas, donde se realizarán preguntas y se responderá con si o no
+ * tras leer las respuestas todas juntas se motrará un mensaje si has ganado o has perdido.
+ */
+public class Preguntas {
+	
+	// Declaramos algunas variables
+	public char res; // esta nos servirá para guardar la respuesta
+	
+		public void Preguntas() {
+
+
 		Scanner lector = new Scanner(System.in);
 		System.out.println("¿Cristóbal Colón descubrió América?");
 		res = lector.next().charAt(0); // Con esto leemos la respuesta
 		
+		
+		
 		if (res == 's') { // Verificamos que la respuesta sea si y pasamos a lo siguiente
-						
+			/**
+			 * En este método comprobamos si la respuesta es <b>correcta</b>.
+			 */
 			System.out.println("¿Java es un lenguaje de programación?");
+		
 			res = lector.next().charAt(0); // Con esto leemos la respuesta
 			if (res == 's') {
 				System.out.println("¿Microsoft Word es un programa de pc?");
+				
+				/**
+				 * En este método comprobamos si la respuesta es <b>incorrecta</b>.
+				 */
 				res = lector.next().charAt(0); // Con esto leemos la respuesta
 				if (res == 's')
 					System.out.println("Felicidades has ganado!");
@@ -27,6 +43,5 @@ class Preguntas { // recuerden que el nombre de la clase es en mayúscula
 				System.out.println("Lo siento ha perdido!");
 		} else
 			System.out.println("Lo siento ha perdido!");
-	}
-	
-}
+	}	
+  }
