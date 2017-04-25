e904586957d4d025e33f4292f6a4ab5c86989409
Guille <Guille@Guille_PT>
2017-04-25 12:17:07 +0200

    Comentarios con html

--- 3Game4U/src/LanzarDados.java
+++ 3Game4U/src/LanzarDados.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class LanzarDados { // Abre clase LanzarDados
+	
+	/**
+	 * <h2>Esto es el inicio de la clase Lanzar dados</h2>
+	 * <p>Aqui creamos una variable y una array llamada "Arreglo".</p>
+	 * <p>Creamos los metodos Scaner el cual lee una entrada por teclado y<br> 
+	 * el metodo Ramdom el cual genera numero aleatorios</p>
+	 * 
+	 * @author @guille
+	 */
 
 	Scanner entrada = new Scanner(System.in);
 	Random aleatorio = new Random();
@@ -11,6 +20,13 @@ public class LanzarDados { // Abre clase LanzarDados
 	/////////////////////////////////////////////
 	// Metodo Recibir
 	/////////////////////////////////////////////
+	
+	/**
+	 * <p>Este meodo se encarga de pedir el numero de veces que quieres<br>
+	 * lanzar el dado y los añade a la array</p>
+	 * 
+	 * @author @guille
+	 */
 
 	public void Recibir() { // Abre metodo Recibir
 		System.out.print("\nPor favor introduzca el numero de veces que se lanzaran ");
@@ -23,6 +39,13 @@ public class LanzarDados { // Abre clase LanzarDados
 	/////////////////////////////////////////////
 	// Metodo Lanzar
 	/////////////////////////////////////////////
+	
+	/**
+	 * <p>Este metodo se encarga de realizar el lanzamiento de los dados<br>
+	 * mediante el metodo Random que creamos arriba</p>
+	 * 
+	 * @author @guille
+	 */
 
 	public void Lanzar() { // Abre metodo Lanzar
 		int numero1;
@@ -34,7 +57,7 @@ public class LanzarDados { // Abre clase LanzarDados
 			// System.out.printf("\n%d\t%d\n", numero1, numero2);
 			// Descomentar para verificar que las sumas se capturan
 			// de manera correcta. Para esto intruducir un numero
-			// pequenio
+			// pequeño
 			Arreglo[i] = numero1 + numero2;
 		} // Cierra for
 
@@ -44,6 +67,13 @@ public class LanzarDados { // Abre clase LanzarDados
 	/////////////////////////////////////////////
 	// Metodo Contar
 	/////////////////////////////////////////////
+	
+	/**
+	 * <p>Este metodo se encarga de recorrer el array mostrando los<br>
+	 * numeros resultantes del metodo Lanzar</p>
+	 * 
+	 * @author @guille
+	 */
 
 	public void Contar() { // Abre metodo Contar
 		int Contador[] = new int[13];
@@ -62,6 +92,13 @@ public class LanzarDados { // Abre clase LanzarDados
 	/////////////////////////////////////////////
 	// Metodo Imprimir
 	/////////////////////////////////////////////
+	
+	/**
+	 * <p>Este metodo Imprime el resultado dependiento de<br>
+	 * la cantidad de veces que quiseramos lanzar el dado</p>
+	 * 
+	 * @author @guille
+	 */
 
 	public void Imprimir(int B[]) { // Abre metodo Imprimir
 		for (int m = 0; m < B.length; m++) { // Abre for
