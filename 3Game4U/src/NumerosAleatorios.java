fcb8c9938ff951554a0d098b4ff59e0a5fd3a8fe
angad <angad@DESKTOP-T7HM9C2>
2017-04-27 09:12:22 +0200

    Ya está todo comentado
    Signed-off-by: aaridom

--- 3Game4U/src/NumerosAleatorios.java
+++ 3Game4U/src/NumerosAleatorios.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
+/**
+ * Clase que se encargará de generar números aleatorios que el usuario tendrá que adivinar.
+ * @author angad
+ * @version <b>1.0</b>
+ */
 public class NumerosAleatorios {
-	
+	/**
+	 * Lista de atributos incluyendo el Scanner 
+	 */
 	Scanner in = new Scanner(System.in);
-	int numero;
-	int valor;
-	int contador=0;
-	boolean ganar=false;
+	private int numero;
+	private int valor;
+	private int contador=0;
+	private boolean ganar=false;
 	
 	/**
 	 * <ul>
@@ -15,21 +22,23 @@ public class NumerosAleatorios {
 	 * 
 	 * Este método creo un número random del 1 al 100 y con un do while se irá repitiendo
 	 * hasta que el usuario acierte el número en la consola.  
-	 * 
-	 * @author aaridom
-	 * @version <b>1.0</b>
 	 */
 	public void aleatorio() {
 		System.out.println("--------------------------------------");
 		System.out.println("|         Bienvenido al juego        |");
 		System.out.println("--------------------------------------");
 		
+		/**
+		 * Generación de números aleatorios del 1 al 100
+		 */
 		numero=1+(int)(Math.random()*100);
 		  System.out.println("En estos momentos se ha generado un numero comprendido entre-\n"+
 		                     "1 y 100, que debes adivinar");
 		  
 		  System.out.println("--------------------------------------");
-		  
+		  /**
+		   * Bucle por el cual el usuario tendrá que poner los números para adivinar el correcto.
+		   */
 		   do{
 		     contador++;
 		     System.out.print("-Escribe el numero ");
